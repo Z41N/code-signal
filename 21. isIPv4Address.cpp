bedcd14050d0525ea590a776e466799b2eddf6c5
Zain <zain.sheraz@gmail.com>
2020-08-12 16:14:20 -0400

    Create 21. isIPv4Address.cpp

---
+++ 21. isIPv4Address.cpp
@@ -0,0 +1,4 @@
+bool isIPv4Address(std::string inputString) {
+ regex r("((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])");
+    return regex_match(inputString, r);
+}
